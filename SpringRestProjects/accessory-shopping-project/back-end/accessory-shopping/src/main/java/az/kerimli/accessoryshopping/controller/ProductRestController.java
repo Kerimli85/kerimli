53c7d1d054f60ecd9ec549113d9a6dc3d6b5e564
Elmir85 <kharimlielmir@gmail.com>
2023-08-07 18:13:28 +0400

    save ve productlarin ekranda gosterilmesi

--- SpringRestProjects/accessory-shopping-project/back-end/accessory-shopping/src/main/java/az/kerimli/accessoryshopping/controller/ProductRestController.java
+++ SpringRestProjects/accessory-shopping-project/back-end/accessory-shopping/src/main/java/az/kerimli/accessoryshopping/controller/ProductRestController.java
@@ -1,12 +1,39 @@
 package az.kerimli.accessoryshopping.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import az.kerimli.accessoryshopping.model.ProductModel;
+import az.kerimli.accessoryshopping.repository.ProductRepo;
+import az.kerimli.accessoryshopping.service.ProductService;
 
 @RestController
 @RequestMapping(path = "/products")
 @CrossOrigin(origins = "*")
 public class ProductRestController {
 
+	@Autowired
+	private ProductRepo productRepo;
+
+	@Autowired
+	private ProductService productService;
+
+	// create
+	@PostMapping
+	public ProductModel save(@RequestBody ProductModel productModel) {
+		return productService.saveProduct(productModel);
+	}
+
+	// read
+	@GetMapping
+	public List<ProductModel> findAll() {
+		return productRepo.findAll();
+	}
 }
--- SpringRestProjects/accessory-shopping-project/back-end/accessory-shopping/src/main/java/az/kerimli/accessoryshopping/service/ProductService.java
+++ SpringRestProjects/accessory-shopping-project/back-end/accessory-shopping/src/main/java/az/kerimli/accessoryshopping/service/ProductService.java
@@ -1,5 +1,21 @@
 package az.kerimli.accessoryshopping.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import az.kerimli.accessoryshopping.model.ProductModel;
+import az.kerimli.accessoryshopping.repository.ProductRepo;
+
+
+@Service
 public class ProductService {
 
+	@Autowired
+	private ProductRepo productRepo;
+	
+	public ProductModel saveProduct(ProductModel productModel) {
+		
+		return productRepo.save(productModel);
+	}
+	
 }
