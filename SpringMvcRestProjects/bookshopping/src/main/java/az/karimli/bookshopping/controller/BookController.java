a90e9d61d0fadc8c6cd328a90a661f618f554276
Elmir <141283712+Kerimli85@users.noreply.github.com>
2024-04-07 21:47:50 +0400

    .

--- SpringMvcRestProjects/bookshopping/src/main/java/az/karimli/bookshopping/controller/BookController.java
+++ SpringMvcRestProjects/bookshopping/src/main/java/az/karimli/bookshopping/controller/BookController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import az.karimli.bookshopping.dao.BookDAO;
 import az.karimli.bookshopping.model.Book;
@@ -22,9 +24,17 @@ public class BookController {
 		model.addAttribute("books", books);
 		return "books";
 	}
+
 	@GetMapping(path = "/books/new")
-	public String openNewBookPage() {
-		
+	public String openNewBookPage(Model model) {
+		Book book = new Book();
+		model.addAttribute("book", book);
 		return "new-book";
+	}
+
+	@PostMapping(path = "/books/new-book-process")
+	public String saveBook(@ModelAttribute(name = "book") Book book) {
+		bookDAO.save(book);
+		return "books";
 	}
 }
